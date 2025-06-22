138ba672efd651806251fa07e1b680f2bbe6b4e3
Jay Wu <85020554+Jay-Wu1015@users.noreply.github.com>
2025-06-22 14:18:18 +0800

    Update 111_minimum-depth-of-binary.cpp
    //add leetcode solution

--- Solution/02_BinaryTree/111_minimum-depth-of-binary.cpp
+++ Solution/02_BinaryTree/111_minimum-depth-of-binary.cpp
@@ -1,1 +1,28 @@
 //create Github question
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        
+        if(!root) return 0;
+
+      
+        int left= sumOfLeftLeaves(root->left);
+        int right=sumOfLeftLeaves(root->right);
+      
+        if( root->left && !root->left->left && !root->left->right)
+            left+= root->left->val; 
+
+        return left+right;
+
+    }
