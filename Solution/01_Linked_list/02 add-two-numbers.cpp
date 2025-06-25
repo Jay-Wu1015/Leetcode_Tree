be1423375be13bed9f8235f70b91b4938dd8c01a
Jay Wu <85020554+Jay-Wu1015@users.noreply.github.com>
2025-06-25 17:13:20 +0800

    Update 02 add-two-numbers.cpp
    Change code

--- Solution/01_Linked_list/02 add-two-numbers.cpp
+++ Solution/01_Linked_list/02 add-two-numbers.cpp
@@ -11,6 +11,33 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //建立虛頭節點
+        ListNode *dummy = new ListNode(0);
+        //建立curr節點，以方便進入loop時操作next，並將dummy指標指的位址複製給curr
+        ListNode *curr= dummy;
         
+        int carry =0;
+
+        //判斷l1, l2 是否為nullptr, carry 是否有值進位，若非則進入loop
+        while(l1 || l2 || carry){
+
+            int sum= carry;
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2){
+                sum += l2 ->val;
+                l2 = l2->next;
+            }
+
+            carry = sum /10;
+            curr->next = new ListNode( sum%10);
+            curr=curr->next;
+
+        }
+
+        return dummy->next;
+    
     }
 };
